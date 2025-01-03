12fd123c40de7f5dc65d03334210d88d03dfb592
Andreas Johansson <glanshammar70@gmail.com>
2025-01-03 11:11:52 +0100

    Added 'Machine' to DBContext

--- BlazorMachineManagement/Data/ApplicationDbContext.cs
+++ BlazorMachineManagement/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using BlazorMachineManagement.Models;
 
 namespace BlazorMachineManagement.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        public DbSet<Machine> Machine { get; set; } = default!;
     }
 }
