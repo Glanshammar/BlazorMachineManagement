0efdebe59b033db32208ffb97ce94b6df1d9c0b7
Andreas Johansson <glanshammar70@gmail.com>
2025-01-03 17:16:24 +0100

    Refactored layout

--- BlazorMachineManagement/Controllers/MachinesController.cs
+++ BlazorMachineManagement/Controllers/MachinesController.cs
@@ -1,108 +1,102 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using BlazorMachineManagement.Data;
 using BlazorMachineManagement.Models;
 
-namespace BlazorMachineManagement.Controllers
+namespace BlazorMachineManagement.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MachinesController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class MachinesController : ControllerBase
+    private readonly ApplicationDbContext _context;
+
+    public MachinesController(ApplicationDbContext context)
     {
-        private readonly ApplicationDbContext _context;
+        _context = context;
+    }
 
-        public MachinesController(ApplicationDbContext context)
+    // GET: api/Machines
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Machine>>> GetMachine()
+    {
+        return await _context.Machine.ToListAsync();
+    }
+
+    // GET: api/Machines/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Machine>> GetMachine(Guid id)
+    {
+        var machine = await _context.Machine.FindAsync(id);
+
+        if (machine == null)
         {
-            _context = context;
+            return NotFound();
         }
 
-        // GET: api/Machines
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<Machine>>> GetMachine()
+        return machine;
+    }
+
+    // PUT: api/Machines/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutMachine(Guid id, Machine machine)
+    {
+        if (id != machine.Id)
         {
-            return await _context.Machine.ToListAsync();
+            return BadRequest();
         }
 
-        // GET: api/Machines/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Machine>> GetMachine(Guid id)
-        {
-            var machine = await _context.Machine.FindAsync(id);
+        _context.Entry(machine).State = EntityState.Modified;
 
-            if (machine == null)
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!MachineExists(id))
             {
                 return NotFound();
             }
-
-            return machine;
-        }
-
-        // PUT: api/Machines/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMachine(Guid id, Machine machine)
-        {
-            if (id != machine.Id)
+            else
             {
-                return BadRequest();
+                throw;
             }
-
-            _context.Entry(machine).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!MachineExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
         }
 
-        // POST: api/Machines
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Machine>> PostMachine(Machine machine)
+        return NoContent();
+    }
+
+    // POST: api/Machines
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Machine>> PostMachine(Machine machine)
+    {
+        _context.Machine.Add(machine);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetMachine", new { id = machine.Id }, machine);
+    }
+
+    // DELETE: api/Machines/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMachine(Guid id)
+    {
+        var machine = await _context.Machine.FindAsync(id);
+        if (machine == null)
         {
-            _context.Machine.Add(machine);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMachine", new { id = machine.Id }, machine);
+            return NotFound();
         }
 
-        // DELETE: api/Machines/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMachine(Guid id)
-        {
-            var machine = await _context.Machine.FindAsync(id);
-            if (machine == null)
-            {
-                return NotFound();
-            }
+        _context.Machine.Remove(machine);
+        await _context.SaveChangesAsync();
 
-            _context.Machine.Remove(machine);
-            await _context.SaveChangesAsync();
+        return NoContent();
+    }
 
-            return NoContent();
-        }
-
-        private bool MachineExists(Guid id)
-        {
-            return _context.Machine.Any(e => e.Id == id);
-        }
+    private bool MachineExists(Guid id)
+    {
+        return _context.Machine.Any(e => e.Id == id);
     }
 }
