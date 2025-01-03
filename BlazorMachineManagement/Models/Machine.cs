23a65bec7e2d586a4026f77d1c6914dd599676f8
Andreas Johansson <glanshammar70@gmail.com>
2025-01-03 11:12:21 +0100

    Added 'Machine' model

---
+++ BlazorMachineManagement/Models/Machine.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorMachineManagement.Models;
+
+public class Machine
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public bool IsOnline { get; set; }
+    public DateTime LastDataSent { get; set; }
+}
