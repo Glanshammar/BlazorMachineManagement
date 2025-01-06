ef62c8fbaac294cc68e5c9c6e66520c2e8b5deed
Andreas Johansson <glanshammar70@gmail.com>
2025-01-06 17:48:34 +0100

    Added data annotations

--- BlazorMachineManagement/Models/Machine.cs
+++ BlazorMachineManagement/Models/Machine.cs
@@ -1,12 +1,27 @@
-﻿namespace BlazorMachineManagement.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMachineManagement.Models;
 
 public class Machine
 {
     public Guid Id { get; set; }
+    
+    [Required]
+    [StringLength(100, MinimumLength = 3)]
     public string Name { get; set; }
+    
+    [Required]
     public bool IsOnline { get; set; }
+    
     public DateTime LastDataSent { get; set; }
+    
+    [Range(-273.15, 1000)]
     public double Temperature { get; set; }
+    
+    [Range(0, int.MaxValue)]
     public int ProductionRate { get; set; }
+    
+    [Required]
+    [Range(0, 1000)]
     public double MaxTemperature { get; set; }
 }
