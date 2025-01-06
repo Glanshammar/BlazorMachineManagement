68778c38e6f6f478be6902b6c2999eecbcd180f9
Andreas Johansson <glanshammar70@gmail.com>
2025-01-06 15:10:45 +0100

    Updated DTO

--- BlazorMachineManagement/DTO/MachineDataDto.cs
+++ BlazorMachineManagement/DTO/MachineDataDto.cs
@@ -2,6 +2,8 @@
 
 public class MachineDataDto
 {
+    public string Name { get; set; }
+    public bool IsOnline { get; set; }
     public double Temperature { get; set; }
     public int ProductionRate { get; set; }
 }
