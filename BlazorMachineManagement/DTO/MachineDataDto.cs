0200771c6223358840013498e8eb1ac9da04a3ad
Andreas Johansson <glanshammar70@gmail.com>
2025-01-06 11:37:25 +0100

    Created base for DTO

---
+++ BlazorMachineManagement/DTO/MachineDataDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorMachineManagement.DTO;
+
+public class MachineDataDto
+{
+    public double Temperature { get; set; }
+    public double Pressure { get; set; }
+    public int ProductionRate { get; set; }
+}
