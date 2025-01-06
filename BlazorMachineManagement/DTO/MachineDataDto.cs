1e3e0f1655d8e4816b3aeb6183be6fdc9e2289ed
Andreas Johansson <glanshammar70@gmail.com>
2025-01-06 17:13:20 +0100

    Added data annotations

--- BlazorMachineManagement/DTO/MachineDataDto.cs
+++ BlazorMachineManagement/DTO/MachineDataDto.cs
@@ -1,9 +1,21 @@
-﻿namespace BlazorMachineManagement.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorMachineManagement.DTO;
 
 public class MachineDataDto
 {
+    [Required]
+    [StringLength(50, MinimumLength = 3)]
     public string Name { get; set; }
+    
+    [Required]
     public bool IsOnline { get; set; }
+    
+    [Required]
+    [Range(-273.15, 1000)]
     public double Temperature { get; set; }
+    
+    [Required]
+    [Range(0, int.MaxValue)]
     public int ProductionRate { get; set; }
 }
